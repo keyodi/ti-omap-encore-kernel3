f0a7473ea0309fd34da078164138f35d4d92ab22
Keyodi <zeusomighty@hotmail.com>
2012-05-27 16:25:57 -0700

    cyttsp: updated ts driver -source used from: https://github.com/torvalds/linux -referenced board changes from: https://github.com/martinezjavier/nook_color/tree/master/cyttsp

--- arch/arm/mach-omap2/board-encore-peripherals.c
+++ arch/arm/mach-omap2/board-encore-peripherals.c
@@ -165,59 +165,43 @@ static struct ft5x06_platform_data ft5x06_platform_data = {
 	.use_gestures = 0,
 };
 
-int cyttsp_dev_init(int resource)
+static int cyttsp_dev_init(void)
 {
-	if (resource) {
-		omap_mux_init_signal("cam_d0.gpio_99", OMAP_PIN_INPUT | OMAP_PIN_OFF_WAKEUPENABLE);
-		omap_mux_init_signal("gpmc_d10.gpio_46", OMAP_PIN_OUTPUT );
-
-		if (gpio_request(OMAP_CYTTSP_RESET_GPIO, "tma340_reset") < 0) {
-			printk(KERN_ERR "can't get tma340 xreset GPIO\n");
-			return -1;
-		}
-
-		if (gpio_request(OMAP_CYTTSP_GPIO, "cyttsp_touch") < 0) {
-			printk(KERN_ERR "can't get cyttsp interrupt GPIO\n");
-			return -1;
-		}
-
-		gpio_direction_input(OMAP_CYTTSP_GPIO);
-		/* omap_set_gpio_debounce(OMAP_CYTTSP_GPIO, 0); */
-	} else {
-		printk ("\n%s: Free resources",__FUNCTION__);
-		gpio_free(OMAP_CYTTSP_GPIO);
-		gpio_free(OMAP_CYTTSP_RESET_GPIO);
+	if (gpio_request(OMAP_CYTTSP_RESET_GPIO, "tma340_reset") < 0) {
+		printk(KERN_ERR "can't get tma340 xreset GPIO\n");
+		return -1;
 	}
+
+	if (gpio_request(OMAP_CYTTSP_GPIO, "cyttsp_touch") < 0) {
+		printk(KERN_ERR "can't get cyttsp interrupt GPIO\n");
+		return -1;
+	}
+
+	gpio_direction_input(OMAP_CYTTSP_GPIO);
+	/* omap_set_gpio_debounce(OMAP_CYTTSP_GPIO, 0); */
+
 	return 0;
 }
 
 static struct cyttsp_platform_data cyttsp_platform_data = {
-        .maxx = 480,
-        .maxy = 800,
-        .flags = 0,
-        .gen = CY_GEN3,
-        .use_st = CY_USE_ST,
-        .use_mt = CY_USE_MT,
-        .use_hndshk = CY_SEND_HNDSHK,
-        .use_trk_id = 1, //CY_USE_TRACKING_ID,
-        .use_sleep = CY_USE_SLEEP,
-        .use_gestures = 0,
-        /* activate up to 4 groups
-         * and set active distance
-         */
-        .gest_set = 0,
-        /* change act_intrvl to customize the Active power state 
-         * scanning/processing refresh interval for Operating mode
-         */
-        .act_intrvl = CY_ACT_INTRVL_DFLT,
-        /* change tch_tmout to customize the touch timeout for the
-         * Active power state for Operating mode
-         */
-        .tch_tmout = CY_TCH_TMOUT_DFLT,
-        /* change lp_intrvl to customize the Low Power power state 
-         * scanning/processing refresh interval for Operating mode
-         */
-        .lp_intrvl = CY_LP_INTRVL_DFLT,
+	.name = CY_I2C_NAME,
+	.init = cyttsp_dev_init,
+	.maxx = 600,
+	.maxy = 1024,
+	.use_hndshk = 0 /*CY_SEND_HNDSHK*/,
+	/* change act_intrvl to customize the Active power state
+	 * scanning/processing refresh interval for Operating mode
+	 */
+	.act_intrvl = CY_ACT_INTRVL_DFLT,
+	/* change tch_tmout to customize the touch timeout for the
+	 * Active power state for Operating mode
+	 */
+	.tch_tmout = CY_TCH_TMOUT_DFLT,
+	/* change lp_intrvl to customize the Low Power power state
+	 * scanning/processing refresh interval for Operating mode
+	 */
+	.lp_intrvl = CY_LP_INTRVL_DFLT,
+	.irq_gpio = OMAP_CYTTSP_GPIO,
 };
 
 /* Encore keymap*/
